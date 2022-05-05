e0a1852c273ca5147c419461aca61fac7f82f884
francescoP02 <file.partipilo@gmail.com>
2022-05-05 16:09:13 +0200

    Adjust scaffolding

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-VTTCue.config.devtools = true;
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+Vue.config.devtools = true;
 
 const app = new Vue(
     {
