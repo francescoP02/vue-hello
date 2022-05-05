3d31cb98c894743845b2bbdfb2d6692317c3bc75
francescoP02 <file.partipilo@gmail.com>
2022-05-05 16:21:43 +0200

    Bonus done

--- js/script.js
+++ js/script.js
@@ -12,14 +12,25 @@ const app = new Vue(
         el: `#root`,
 
         data: {
+
+            userMessage: "red",
+
             userName: prompt("Qual è il tuo nome?"),
 
             userSurname: prompt("Qual è il tuo cognome?"),
 
+            myImage: "https://picsum.photos/246/146",
         },
 
         methods: {
-
+            changeColor:function(event) {
+                event.preventDefault();
+                if (this.userMessage === `blue`) {
+                    this.userMessage = `red`;
+                } else {
+                    this.userMessage = `blue`;
+                }
+            },
         },
     }
 )
